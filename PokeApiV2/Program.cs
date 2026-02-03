4ec5555bd3b524262c2fb7e9baa1f3910ad92c4c
Unai Villar <unaidevcoding@gmail.com>
2026-02-03 18:08:38 +0100

    edited readme

--- PokeApiV2/Program.cs
+++ PokeApiV2/Program.cs
@@ -3,13 +3,13 @@ using PokeApiV2.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// === Services Area ===
+// ==== Services Area ====
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("name=DefaultConnection"));
 
 var app = builder.Build();
 
-// === Middlewares Area ===
+// ==== Middlewares Area ====
 app.MapControllers();
 app.Run();
